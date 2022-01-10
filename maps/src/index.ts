c80a80c0be363d8077d6c2302da9451acaba1c58
Michael <fourjustoneday@yandex.ru>
2022-01-10 16:17:01 +0300

    app3

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -4,3 +4,11 @@ import { User } from "./User";
 
 const u = new User();
 console.log(u);
+
+new google.maps.Map(document.getElementById('map'), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
