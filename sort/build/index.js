5d676351b9897a762048d992c5f1d1a98a7d4068
Michael <fourjustoneday@yandex.ru>
2022-01-12 15:33:26 +0300

    scripting

---
+++ sort/build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("13 penis");
+const log = () => {
+    console.log("123");
+};
---
+++ sort/src/index.ts
@@ -0,0 +1,5 @@
+console.log("13 penis");
+
+const log = () => {
+    console.log("123");
+}
