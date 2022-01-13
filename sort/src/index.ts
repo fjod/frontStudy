1ebb5ad34d8fe67df22aa1993e3005125673d0f5
Michael <fourjustoneday@yandex.ru>
2022-01-13 15:31:15 +0300

    app5 - same class/inteface/abstract class things as in C#

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,5 +1,29 @@
-console.log("13 penis");
+class Sorter{
 
-const log = () => {
-    console.log("123");
+    constructor(public collection:number[] | string) {    }
+
+    sort() :void{
+        if (this.collection instanceof Array)
+        {
+        for (let i =0; i< this.collection.length; i++){
+            for (let j = 0; j< this.collection.length - i -1; j++){
+                if (this.collection[j] > this.collection[j+1]){
+
+                        const temp = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = temp;
+
+                }
+            }
+        }
+        }
+        if (this.collection instanceof String){
+
+        }
+    }
 }
+
+const sorter = new Sorter([190,3,-5,0]);
+
+sorter.sort();
+console.log(sorter.collection);
